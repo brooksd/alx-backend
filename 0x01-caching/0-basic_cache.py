9c6e99dda2a42a9a82f2951c361e720fd6724842
Donald Brooks <brooksd079@gmail.com>
2023-05-17 13:22:34 +0300

    Overrides put() and self() from parent

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Module 0-basic_cache """
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ Overrides put() and self() from parent """
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item by key """
+        value = self.cache_data.get(key)
+        return value
