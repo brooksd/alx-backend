fe1be6be026a2955e8849d097d115004d485537d
Donald Brooks <brooksd079@gmail.com>
2023-06-20 13:33:35 +0300

    Queue that loops through a job array

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -3,7 +3,7 @@ const kue = require('kue');
 const queue = kue.createQueue();
 
 const data = {
-  phoneNumber: '0716630090',
+  phoneNumber: '4153518780',
   message: 'This is the code to verify your account',
 };
 
